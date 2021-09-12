c1c1f858a6e84e490f1d1b9afaf533ee318362a0
martinmazas <mazasmartin@gmail.com>
2021-09-12 15:49:04 +0300

    Update Pad.js

--- src/Components/Pad.js
+++ src/Components/Pad.js
@@ -4,7 +4,6 @@ import Container from '@material-ui/core/Container';
 import MediaControlCard from './MediaControlCard';
 import Square from './Square';
 import allLoops from '../Data/samples.json';
-import RecordLoop from './RecordLoop';
 
 const styles = {
     wrapper: {
@@ -34,7 +33,6 @@ export default function Pad() {
             <CssBaseline />
             <Container fixed style={styles.wrapper}>
                 <MediaControlCard loops={loops.filter(loop => loop.state === 'on')} setPlaying={setPlaying} />
-                <RecordLoop />
                 <div style={styles.squares}>
                     {loops.map((loop => (
                         <Square key={loop.id} loop={loop} changeLoopState={changeLoopState} playing={playing} setPlaying={setPlaying} />
