38ecb6c1120c2178303e2a1bea9cfba0afea7b7d
martinmazas <mazasmartin@gmail.com>
2021-09-12 15:46:23 +0300

    .

--- src/Components/MediaControlCard.js
+++ src/Components/MediaControlCard.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import IconButton from '@material-ui/core/IconButton';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import StopIcon from '@material-ui/icons/Stop';
 import MicIcon from '@material-ui/icons/Mic';
-import AudioReactRecorder, { RecordState } from 'audio-react-recorder';
+// import AudioReactRecorder, { RecordState } from 'audio-react-recorder';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -38,7 +38,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function MediaControlCard(props) {
   const classes = useStyles();
-  const [recordState, setRecordState] = useState(null);
+  // const [recordState, setRecordState] = useState(null);
 
   return (
     <Card className={classes.root}>
